5d481cd04965770e97144910339f1c6299a8631c
Eli <jo45840@hotmail.com>
2020-05-22 18:45:19 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 const PORT = process.env.PORT || 3000
