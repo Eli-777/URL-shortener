0bd79bf15bf89f144dd37655295f226d19107120
Eli <jo45840@hotmail.com>
2020-05-22 16:22:21 +0800

    feat: add show page

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.listen(PORT, () =>{
+app.post('/', (req, res) => {
+  console.log('body = ', req.body)
+  const url = req.body.url
+  res.render('show', { url })
+})
+
+
+app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
