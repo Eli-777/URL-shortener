466d3fbb8a577c9a452ae35e1f75918e4993de32
Eli <jo45840@hotmail.com>
2020-05-23 00:28:42 +0800

    feat: add find duplicate short url function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -7,36 +7,33 @@ const generateShortURL = require('../../generate_shortURL')
 router.get('/', (req, res) => {
   res.render('index')
 })
-//建立短網址
+// 建立短網址
 router.post('/', (req, res) => {
   url = req.body.url
   if (url.includes('https://') || url.includes('http://')) {
   } else {
     url = 'https://' + url
   }
+  let short_name = generateShortURL()
  
   return Url.find()
     .then(check => {
-      console.log(check)
+      // 需要防止有重覆的網址組合出現
       let created = []
       check.forEach(item => {
         created.push(item.short_name)
       })
-      console.log(created)
       while (created.includes(short_name)) {
-        console.log('while working')
         short_name = generateShortURL()
       }
-      console.log('short = ',short_name)
       return short_name
     })
-    .then(test => console.log('test = ', test))
     .then(() => Url.create({ short_name, url }))
     .then(() => res.render('show', { url, short_name }))
     .catch(error => console.log(error))
 })
 
-//短網址轉跳
+// 短網址轉跳
 router.get('/:id', (req, res) => {
   const id = req.params.id
   Url.find({ short_name: id })
